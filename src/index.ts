0a9c0546b8cc93684e0c68e456d21eaaced7121b
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-10-19 18:40:21 -0300

    req13:feito

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Battle, { PVP, PVE } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Jon');
+const player2 = new Character('Robb');
+const player3 = new Character('Theon');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]): void => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
